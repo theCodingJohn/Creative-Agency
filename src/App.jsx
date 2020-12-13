9f81cd9bd13bbb757cdd4e4fd4e361ffee1d1e1a
John Carlo Cunanan <codingjohn@outlook.com>
2020-12-13 22:56:34 +0800

    add navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from './components/Nav/Nav';
+
+// styles
+import "./app.css";
+
+function App() {
+  return (
+    <>
+      <Nav/>
+    </>
+  );
+}
+
+export default App;
